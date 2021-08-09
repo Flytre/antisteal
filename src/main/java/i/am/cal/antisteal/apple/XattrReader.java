0d1c7a68441131f47283cf8d66b8140d73df3b6f
cal6541 <44925698+i-am-cal@users.noreply.github.com>
2021-08-09 21:27:51 +0100

    Detach from xattrJ

--- src/main/java/i/am/cal/antisteal/apple/XattrReader.java
+++ src/main/java/i/am/cal/antisteal/apple/XattrReader.java
@@ -1,9 +1,12 @@
 package i.am.cal.antisteal.apple;
 
-import ch.securityvision.xattrj.Xattrj;
-
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.Properties;
 
 public class XattrReader {
@@ -24,15 +27,21 @@ public class XattrReader {
     }
 
     public Properties read() {
-        Xattrj xattrj;
         Properties prop = new Properties();
+        UserDefinedFileAttributeView view = Files.getFileAttributeView(_path, UserDefinedFileAttributeView.class);
+        ByteBuffer buffer = null;
         try {
-            xattrj = new Xattrj();
-            String loc = xattrj.readAttribute(_path.toFile(), "kMDItemWhereFroms");
-            prop.setProperty("HostUrl", loc);
-        } catch (Exception e) {
+            buffer = ByteBuffer.allocate(view.size("com.apple.metadata:kMDItemWhereFroms"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        try {
+            view.read("com.apple.metadata:kMDItemWhereFroms", buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        buffer.flip();
+        prop.setProperty("HostUrl", Charset.defaultCharset().decode(buffer).toString());
         return prop;
     }
 }
